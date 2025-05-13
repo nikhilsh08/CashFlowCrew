b3e7bab0fb81bdb41ba59e0abb29d074fef105b6
anurag5764 <danurag983@gmail.com>
2025-05-14 00:18:54 +0530

    disclaimer added

--- src/components/Disclaimer.tsx
+++ src/components/Disclaimer.tsx
@@ -5,8 +5,8 @@ export const Disclaimer: React.FC = () => (
     <div className="max-w-4xl mx-auto px-4 text-center">
       <h2 className="text-3xl font-bold mb-8">Disclaimer:</h2>
       <p className="text-lg mb-16">
-        The information provided in this finance and stock marketing course is
-        for educational purposes only and should not be construed as investment
+        The information provided in this finance and mutual fund course is for
+        educational purposes only and should not be construed as investment
         advice. For personalized investment advice, please consult a
         SEBI-registered Registered Investment Advisor (RIA).
       </p>
