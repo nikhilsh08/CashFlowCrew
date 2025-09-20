55a299f0c732ba6b64ab363f35bd055bd5fefe1a
Ashish Gupta <gkashish185@gmail.com>
2025-09-20 18:12:35 +0530

    remove contact number

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -77,8 +77,8 @@ const ContactUs: React.FC = () => {
               <div className="flex items-start gap-4">
                 <Phone className="w-6 h-6 text-indigo-600 mt-1" />
                 <div>
-                  <h3 className="font-semibold text-gray-900">Phone</h3>
-                  <p className="text-gray-600">+91-9902366044</p>
+                  {/* <h3 className="font-semibold text-gray-900">Phone</h3>
+                  <p className="text-gray-600">+91-9902366044</p> */}
                 </div>
               </div>
             </div>
